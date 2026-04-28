3173343f5f0c005415848406ff5fdafbaeae286f
AzizKanchwala <azizkanch435@gmail.com>
2026-04-28 09:42:45 +0530

    Add macros and use them in marts

--- models/marts/eth_activity_per_day.sql
+++ models/marts/eth_activity_per_day.sql
@@ -2,7 +2,7 @@ select
 date,
 transaction_category,
 count(*) as tx_count,
-sum(value)/1e18 as sum_eth_value
+{{ conversion('value', '18') }} as sum_eth_value
 
 from {{ ref('stg_transactions_enriched') }}
 
