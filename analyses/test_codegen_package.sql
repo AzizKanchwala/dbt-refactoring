90ee617bc5eaa7fc16714521dbaa7be38a7ce4df
AzizKanchwala <azizkanch435@gmail.com>
2026-04-28 13:41:04 +0530

    Add codegen package and analysis file to compile

---
+++ analyses/test_codegen_package.sql
@@ -0,0 +1,2 @@
+{{ codegen.generate_source(schema_name='eth_schema', database_name='eth', generate_columns = True) }}
+{{ codegen.generate_model_yaml(['stg_transactions', 'stg_transactions_enriched', 'stablecoin_activity_per_day']) }}
