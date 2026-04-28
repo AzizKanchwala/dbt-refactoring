eacec6fe0b3e632fe31f44c7631562e9a7413428
AzizKanchwala <azizkanch435@gmail.com>
2026-04-28 10:20:47 +0530

    Restore stablecoin model

--- models/marts/stablecoin_activity_per_day.sql
+++ models/marts/stablecoin_activity_per_day.sql
@@ -6,8 +6,7 @@ token_address,
 from {{ ref('stg_token_transfers') }}
 
 
-where lower(token_address) in ('0xdac17f958d2ee523a2206206994597c13d831ec7', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48') 
-or lower(token_address) in ( {{random_macro() }} )
+where lower(token_address) in ('0xdac17f958d2ee523a2206206994597c13d831ec7', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
 
 group by 
 date,
