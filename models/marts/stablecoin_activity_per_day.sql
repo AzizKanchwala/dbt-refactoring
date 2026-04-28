761211d51499cfeb727067d3961407d29436aab4
AzizKanchwala <azizkanch435@gmail.com>
2026-04-28 10:12:13 +0530

    Use the random_macro returning the token_addresses as a filter in stablecoin_activity .. mart

--- models/marts/stablecoin_activity_per_day.sql
+++ models/marts/stablecoin_activity_per_day.sql
@@ -6,7 +6,7 @@ token_address,
 from {{ ref('stg_token_transfers') }}
 
 
-where lower(token_address) in ('0xdac17f958d2ee523a2206206994597c13d831ec7', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
+where lower(token_address) in {{random_macro()}}
 
 group by 
 date,
