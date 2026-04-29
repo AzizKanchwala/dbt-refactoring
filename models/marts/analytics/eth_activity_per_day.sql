28df2944f1e08e88861fddaba205ea3457c03422
AzizKanchwala <azizkanch435@gmail.com>
2026-04-29 15:29:14 +0530

    Add manifest to state folder and play around to test

--- models/marts/analytics/eth_activity_per_day.sql
+++ models/marts/analytics/eth_activity_per_day.sql
@@ -2,7 +2,7 @@ select
 date,
 transaction_category,
 count(*) as tx_count,
-{{ conversion('value', '18') }} as sum_eth_value
+{{ conversion('value', '18') }} as sum_eth_value_state
 
 from {{ ref('stg_transactions_enriched') }}
 
---
+++ models/marts/fraud/test_state.sql
@@ -0,0 +1,3 @@
+select 1 as test
+-- Run this with dbt run -m state:new --state state
+-- This is different from dbt run -m state:modified --state state
