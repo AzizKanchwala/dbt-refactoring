02665af3c91b8ab9bdf6d138b7a789e62a78366a
AzizKanchwala <azizkanch435@gmail.com>
2026-04-29 18:00:41 +0530

    Add changes to trigger the ci pipeline

--- models/marts/analytics/eth_activity_per_day.sql
+++ models/marts/analytics/eth_activity_per_day.sql
@@ -2,7 +2,7 @@ select
 date,
 transaction_category,
 count(*) as tx_count,
-{{ conversion('value', '18') }} as sum_eth_value_state
+{{ conversion('value', '18') }} as sum_value_ethereum
 
 from {{ ref('stg_transactions_enriched') }}
 
